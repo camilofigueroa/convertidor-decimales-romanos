926f03943addb04af8a998ff35763ff90cecf83e
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2018-08-25 12:27:13 -0500

    Versión más sólida
    Aquí ya se convierten una gran cantidad de números. Puede que haya algunos casos que no.

--- romanos.js
+++ romanos.js
@@ -10,12 +10,13 @@ var g_texto_original = "";
 //Ojo, de primera mano el órden importa.
 //La tercera columna me indica cuantas casillas debo ir adelante en el orden del vector
 //para convertir un numero que en romano usará cifras anteriores.
-var g_arreglo = [   [ "500", "D", 1 ],
-                    [ "100", "C", 2 ],                    
-                    [ "50",  "L", 1 ],
-                    [ "10",  "X", 2 ],
-                    [ "5",   "V", 1 ],
-                    [ "1",   "I", 0 ]                    
+var g_arreglo = [   [ "1000", "M", 1 ],
+                    [ "500",  "D", 1 ],
+                    [ "100",  "C", 2 ],                    
+                    [ "50",   "L", 1 ],
+                    [ "10",   "X", 2 ],
+                    [ "5",    "V", 1 ],
+                    [ "1",    "I", 0 ]                    
                 ];
 
 /**
@@ -42,6 +43,7 @@ function al_dar_clic()
 function operar()
 {
     var i = 0, j = 0;  //j es para un segundo ciclo.
+    var k = 0;
     var numero = g_texto_original.value * 1;
     var salida = "";
     var tmp_num = 0;
@@ -51,7 +53,7 @@ function operar()
 
     //Este ciclo es tentativo, que haga las cosas algunas veces. Cuando se ataquen todas las cifras...
     //...del número y quede en cero, este ciclo será un while.
-    for( j = 0; j < 3; j ++ ) 
+    for( j = 0; j < 10; j ++ ) 
     {
         console.log( "Ini ciclo ---- " + numero + " ------ " );
 
@@ -61,7 +63,7 @@ function operar()
             console.log( tmp_num + " " + g_arreglo[ i ][ 1 ] ); //Análisis.
             
             //Parece ser que un buen candidato de número es el que está entre 0.8 y 1.
-            if( tmp_num >= 0.8 && tmp_num < 1 )
+            if( tmp_num >= 0.8 && tmp_num < 1 && ( numero == 4 || numero % 4 != 0 ) )
             {   //Aquí el número que importa es el anterior o anteriores a la cifra encontrada.
                 tmp_num_adelanto = g_arreglo[ i ][ 2 ];
                 salida += g_arreglo[ i + tmp_num_adelanto ][ 1 ] + g_arreglo[ i ][ 1 ];
@@ -72,9 +74,30 @@ function operar()
 
             if( tmp_num == 1 ) //Ojo, encontró el número exacto.
             {
-                salida = g_arreglo[ i ][ 1 ];
+                salida += g_arreglo[ i ][ 1 ];
                 numero = numero - ( g_arreglo[ i ][ 0 ] * 1 );
+                console.log("   -> " + numero );
                 break; //Hay que detener el ciclo porque encontró el número exacto.
+            }
+
+            if( tmp_num >= 1 && tmp_num < 2 )
+            {                   
+                salida += g_arreglo[ i ][ 1 ];
+                numero = numero - ( g_arreglo[ i ][ 0 ] * 1 );
+                console.log("   -> " + numero );
+                break; 
+            }
+
+            //Estos son los números de 2 o 3 cifras que no están en el vector. II III XX XXX CC CCC 
+            if( tmp_num >= 2 && tmp_num <= 3.4 )
+            { 
+                for( k = 1; k <= tmp_num; k ++ ) 
+                {
+                    salida += g_arreglo[ i ][ 1 ]; 
+                    numero = numero - ( g_arreglo[ i ][ 0 ] * 1 );
+                }
+                
+                break;
             }
 
             //salida = salida.replace( g_arreglo[ i ][ 0 ], g_arreglo[ i ][ 1 ] );   
